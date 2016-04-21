74c2b9ed0a174ccb3499b8df9da9b905a6cbfa22
Patricio Vergara Paredes <Patricio Vergara Paredes>
2016-04-21 15:09:48 +0200

    Starting with controllers and templates folders in the www folder

--- www/app.js
+++ www/app.js
@@ -8,3 +8,18 @@ angular.module("myApp", [
   'ngTouch',
   'ui.bootstrap'
 ]);
+
+app.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
+
+	$routeProvider
+    .when("/", {
+      templateUrl: "templates/home.html",
+      controller: "home"
+    })
+    .otherwise({
+      templateUrl: "templates/404.html"
+    });
+
+    $locationProvider.html5Mode(true);
+    
+}]);
