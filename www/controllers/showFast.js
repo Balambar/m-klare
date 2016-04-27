8e914f4f5e0e4d5b658024a87fd0e4118c719f94
Jarl Holmberg Lindquist <jarllindquist@gmail.com>
2016-04-27 14:47:57 +0200

    cleard array in showFast

--- www/controllers/showFast.js
+++ www/controllers/showFast.js
@@ -1,6 +1,9 @@
 app.controller("fast", ["$scope", function($scope) {
   
-	images = ["C:\Users\Jarl\Desktop\dev\uppgifter\projekt\git\macklare\www\imgs\objekt\int\bed.jpg"];
+	images = [
+	
+
+	];
   
 
 }]);
