5e1b6d0d01b6576e238dcadb117a2c89d4011b98
Mayra Rivas <mayrarivas1@hotmail.com>
2016-04-29 16:08:48 +0200

    Ändrad mongo-model och seller

--- mongoose-models/seller.js
+++ mongoose-models/seller.js
@@ -3,10 +3,11 @@ module.exports = function(mongoose){
   // Create a new mongoose schema
   var SellerSchema = mongoose.Schema({
     name: {type: String, required: true},
+    position: {type: String, required: true},
     phone: {type: Number, required: true},
     email: {type: String, required: true},
     about: {type: String, required: true},
-    img: {type: String, required: false}
+    img: {type: String, required: false} 
   });
 
   //enforce the schema required key even when the PUT method is used
