d1759b83bdc0e4a2b49e1acbd98f08c4157d1efa
Jarl Holmberg Lindquist <jarllindquist@gmail.com>
2016-05-03 18:29:13 +0200

    change name from appartment to logenheter and create new dircetive

--- www/controllers/fastigheter.js
+++ www/controllers/fastigheter.js
@@ -1,6 +1,5 @@
 app.controller("fastigheter", ["$scope", function($scope) {
   
-	 $scope.pets = ["Anna Hund","Jacob Kanin","Micke Mus"];
 
  
 }]);
---
+++ www/directives/subNavObject.js
@@ -0,0 +1,16 @@
+app.directive('subNav', [function(){
+	// Runs during compile
+	return {
+		templateUrl: '/directives/subNavObject.html'
+    // controller: ['$scope', 'Download', function($scope, download) {
+//  Download.get({
+//   species: "rabbit",
+//   _sort: {name: 1},
+//   _skip: 10,
+//   _limit: 5
+// });
+ 
+    // }]
+  // };
+	};
+}]);
