0e2c1aa5d591460f2ca68ba4656bc1ba8a79dbe7
Fajaan <fajaanmac@gmail.com>
2016-05-10 03:01:48 +0200

    lade till extra i AboutSchema

--- mongoose-models/about.js
+++ mongoose-models/about.js
@@ -4,8 +4,12 @@ module.exports = function(mongoose) {
 	var AboutSchema = mongoose.Schema({
 
 		title: {type: String, required: true},
+		titles: {type: String, required: true},
 		content: {type: String, required: true},
-		img: {type: String, required: true}
+		contents: {type: String, required: true},
+		img: {type: String, required: true},
+		imgs: {type: String, required: true}
+
 	});
 
 	AboutSchema.pre('update', function(next){
@@ -17,3 +21,4 @@ module.exports = function(mongoose) {
 	return mongoose.model("About", AboutSchema);
 
 }
+	
