dd7c682d2c1a618dd2ec67364006084b2aa0e25f
Martin Nilsson <martinnilss@gmail.com>
2016-05-11 13:42:34 +0200

    minor changes

--- www/directives/enfastInfo.js
+++ www/directives/enfastInfo.js
@@ -1,25 +1,27 @@
 // loads in ui bootstrap accordion
 app.directive('enfastInfo', [function () {
 
+  function boolChange(myData){
+    
+    var myBoolChange;
+    if(myData){
+      myBoolChange = 'yes';
+    }
+    else{
+      myBoolChange = 'no';
+    }
+
+    return myBoolChange;
+  };
+
   return {
     templateUrl: '/directives/enfastInfo.html',
     controller: ['$scope', '$routeParams', 'Home', function($scope, $routeParams, Home) {
       
       $scope.info = Home.getById({id:$routeParams.id}, function(data){
 
-        if(data.garden){
-          $scope.info.garden = 'yes';
-        }
-        else{
-          $scope.info.garden = 'no';
-        }
-
-        if(data.balcony){
-          $scope.info.balcony = 'yes';
-        }
-        else{
-          $scope.info.balcony = 'no';
-        }
+        $scope.info.garden = boolChange(data.garden);
+        $scope.info.balcony = boolChange(data.balcony);
 
         $scope.info.size += ' M\xB2';
         $scope.info.price += ' SEK';
