d403985d03fe7917d310a97f8a42abf1d0851f09
Martin Nilsson <martinnilss@gmail.com>
2016-05-12 16:15:29 +0200

    changes

--- www/directives/myMap.js
+++ www/directives/myMap.js
@@ -3,8 +3,16 @@ app.directive('myMap', [function () {
   return {
     templateUrl: '/directives/myMap.html',
     controller: ['$scope', function($scope) {
-      
+
+    	$scope.map = {
+    center: [40.7, -74]
+  }
+  $scope.marker = {
+    position: [40.7, -74]
+  }
+     setTimeout(function(){window.dispatchEvent(new Event('resize'))},0);
       
     }]
+
   };
 }]);
