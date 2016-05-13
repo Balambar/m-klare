644f3e611e7c196c4cc5deb6f6166e57e505523d
Mayra Rivas <mayrarivas1@hotmail.com>
2016-05-13 13:59:33 +0200

    mean teacher removing bootstrap js! :D

--- www/directives/headerCarousel.js
+++ www/directives/headerCarousel.js
@@ -2,7 +2,18 @@ app.directive('headerCarousel', [function(){
 
 	return {
     templateUrl: '/directives/headerCarousel.html',
-    // controller: ['$scope', '$interval', function($scope, $interval) {
+    controller: ['$scope', '$interval', function($scope, $interval) {
+    	$scope.imgs = [
+    		"imgs/Home/bild1.jpg",
+    		"imgs/Home/bild2.jpg",
+    		"imgs/Home/bild3.jpg",
+    		"imgs/Home/bild4.jpg",
+    		"imgs/Home/bild5.jpg"
+    	];
 
+    	$scope.myInterval = 5000;
+	    $scope.noWrapSlides = false;
+	    $scope.active = 0;
+    }]
   };
 }]);
