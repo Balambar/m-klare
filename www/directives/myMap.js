0ce90b6ee21846170cdcd74c6af2cc724de995bf
Martin Nilsson <martinnilss@gmail.com>
2016-05-13 09:55:06 +0200

    added som random coordinates

--- www/directives/myMap.js
+++ www/directives/myMap.js
@@ -4,14 +4,54 @@ app.directive('myMap', [function () {
     templateUrl: '/directives/myMap.html',
     controller: ['$scope', function($scope) {
 
-    	$scope.map = {
-    		center: [40.7, -74]
-  		}
+    	var coordinates = [
+    		{
+    			lat: 55.5899,
+    			lon: 12.9212
+    		},
+    		{
+    			lat: 55.6109,
+    			lon: 12.9946
+    		},
+    		{
+    			lat: 55.6048,
+    			lon: 12.9874
+    		},
+    		{
+    			lat: 59.3473,
+    			lon: 18.0238
+    		},
+    		{
+    			lat: 58.0367,
+    			lon: 14.3127
+    		}
+    	];
+
+    	function getRandomItem(arr){
+            return arr[Math.floor(Math.random()*arr.length)];
+      	}
+
+      	var myCords = getRandomItem(coordinates);
+
+      	$scope.marker = {
+    		position: [myCords.lat, myCords.lon]
+  		};
   		
-  		$scope.marker = {
-    		position: [40.7, -74]
-  		}
-     setTimeout(function(){window.dispatchEvent(new Event('resize'))},0);
+    	$scope.map = {
+    		center: [myCords.lat, myCords.lon],
+    		options: function(){
+    			return{
+    				scrollwheel: false,
+    				zoom: 15,
+    				draggable: false
+    			}
+    		}
+  		};
+  		
+  		
+     setTimeout(function(){
+     	window.dispatchEvent(new Event('resize'));
+     },0);
       
     }]
 
