d7694e6c1d6992269a8298c890e65d9d84352c3f
Patricio Vergara Paredes <Patricio Vergara Paredes>
2016-05-18 13:56:33 +0200

    Minor changes. Mayra and Patricio

--- www/directives/homeSearch.js
+++ www/directives/homeSearch.js
@@ -8,6 +8,9 @@ app.directive('homeSearch', ['$rootScope', '$location', 'Home', function ($rootS
       if ($location.path() == "/") {
         elem.find('.dropdown-menu').addClass('dropdown-menu-center');
       }
+      else if ($location.path() == "/fastigheter" || $location.path() == "/villor" || $location.path() == "/lagenheter") {
+        elem.find('.dropdown-menu').addClass('dropdown-menu-right');
+      }
 
       // A function to show/hide wells
       scope.toggleDropdown = function(){
