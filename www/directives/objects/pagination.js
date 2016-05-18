6c540dc88393a8f68db2c3c9ae8364d4bc9a57e5
Jarl Holmberg Lindquist <jarllindquist@gmail.com>
2016-05-18 08:58:53 +0200

    stuff

--- www/directives/objects/pagination.js
+++ www/directives/objects/pagination.js
@@ -26,71 +26,30 @@ app.directive("paginationList", [
 
           $scope.all = function() {
             Home.get(function(data) {
-              // console.log(data);
-              // $scope.totalItems = 64;  
-              // $scope.currentPage = 4;
-
-              // $scope.setPage = function(pageNo) {
-
-              //   $scope.currentPage = pageNo;
-              // };
-
-
-              $scope.consol = function(page){
-                console.log(page-1);
-              }
-
+            
               $scope.maxSize = 5;  // how many that shows in the menu  (Limit number for pagination size.)
               $scope.bigTotalItems = data.length;  //total number of objects in db. 
               $scope.bigCurrentPage = 1; // startingpoint for active 
               
 
-              // var numberArray = [];
-            //   var fullNumber = Math.ceil(data.length / 6);
-            //   // console.log(fullNumber);
-            //   for (var i = 1; i <= fullNumber; i++) {
-            //     numberArray.push(i);
-            //     // console.log(numberArray);
-            //   }
-            //   // console.log(fullNumber);
-            //   $scope.first = 0;
-            //   $scope.last = fullNumber;
-            //   $scope.pageNumbers = numberArray;
-            //   // [1,2,3,4,5];
             })
           };
           $scope.villor = function() {
             Home.get({
               type: 'Villor'
             }, function(data) {
-              // console.log(data);
-              var numberArray = [];
-              var fullNumber = Math.ceil(data.length / 6);
-              // console.log(fullNumber);
-              for (var i = 1; i <= fullNumber; i++) {
-                numberArray.push(i);
-                // console.log(numberArray);
-              }
-              // console.log(fullNumber);
-              $scope.pageNumbers = numberArray;
-              // [1,2,3,4,5];
+                $scope.maxSize = 5;  // how many that shows in the menu  (Limit number for pagination size.)
+              $scope.bigTotalItems = data.length;  //total number of objects in db. 
+              $scope.bigCurrentPage = 1; // startingpoint for active 
             })
           };
           $scope.lagenheter = function() {
             Home.get({
               type: 'Lägenhet'
             }, function(data) {
-              // console.log(data);
-              var numberArray = [];
-              var fullNumber = Math.ceil(data.length / 6);
-              // console.log(fullNumber);
-              for (var i = 1; i <= fullNumber; i++) {
-                numberArray.push(i);
-                // console.log(numberArray);
-              }
-              // console.log(fullNumber);
-              $scope.pageNumbers = numberArray;
-              // [1,2,3,4,5];
+               $scope.maxSize = 5;  // how many that shows in the menu  (Limit number for pagination size.)
+              $scope.bigTotalItems = data.length;  //total number of objects in db. 
+              $scope.bigCurrentPage = 1; // startingpoint for active 
             })
           };
           $scope.badge();
