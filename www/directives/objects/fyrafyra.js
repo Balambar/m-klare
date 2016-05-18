d4fe2dec3070cb6f81ee07c128e1a460a7be7633
Balambar <ak_starocean@hotmail.com>
2016-05-18 13:14:00 +0200

    Creating widget jason & jarl

---
+++ www/directives/objects/fyrafyra.js
@@ -0,0 +1,10 @@
+app.directive('fyraFyra', [function(){
+	return {
+		templateUrl: 'directives/objects/fyrafyra.html',
+		link: function(scope, elem, attrs) {
+			elem.find( "#datepicker" ).datepicker();
+		} 
+
+	}
+
+}]);
