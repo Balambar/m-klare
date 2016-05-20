2a441520ea45230a36563210a42fe0d600766019
Martin Nilsson <martinnilss@gmail.com>
2016-05-21 00:30:55 +0200

    map fixed, now placing markers and centers with the coordinates from database

--- www/directives/detailedObject/myMap.js
+++ www/directives/detailedObject/myMap.js
@@ -2,32 +2,57 @@ app.directive('myMap',  [function () {
 
   return {
     templateUrl: '/directives/detailedObject/myMap.html',
-    controller: ['$scope', '$routeParams', 'Home', function($scope, $routeParams, Home) {
+    controller: ['$scope', 'uiGmapGoogleMapApi', '$routeParams', 'Home', function($scope, uiGmapGoogleMapApi, $routeParams, Home) {
 
+      //coordinates that will be loaded from database
       var coordinates = {};
 
+      //initial map setup (the map need these values to load)
+      $scope.map = {
+        center:{
+          latitude: 0, 
+          longitude: 0
+        }, 
+        zoom: 18
+      };
+
+      //initial marker setuo (map need these values to load)
+      $scope.marker = {
+        id: 0,
+        coords: {
+          latitude: coordinates.latitude,
+          longitude: coordinates.longitude
+        },
+        options: { dragable: false }
+      };
+
+      //GET request to database using the routeparam id
     	Home.getById({id:$routeParams.id}, function(data){
 
+        //set the coordinates to the correspoding object from database
         coordinates = data.coordinates[0];
+
+        //when the map api is ready, set the map and marker to the ones loaded from database
+        uiGmapGoogleMapApi.then(function(maps) {
+          $scope.map = {
+            center:{
+              latitude: coordinates.latitude, 
+              longitude: coordinates.longitude
+            }, 
+            zoom: 18
+          };
+
+          //as above
+          $scope.marker = {
+            id: 0,
+            coords: {
+              latitude: coordinates.latitude,
+              longitude: coordinates.longitude
+            },
+            options: { dragable: false }
+          };
+        });
       });
-
-      $scope.map = {
-          center:{
-            latitude: 55.5899, 
-            longitude: 12.9212
-          }, 
-          zoom: 18
-        };
-
-      
-      
-      //this timeout fixes a bug that causes the map to turn grey 
-  		//if the whole page isnt reloaded
-  	 	setTimeout(function(){
-  	 		window.dispatchEvent(new Event('resize'));
-  	 	},0);
-	      
-	  }]
-
-	};
+    }]
+  };
 }]);
