cd0cb938c91ee65c6c743754ae7114f6332bd21b
Martin Nilsson <martinnilss@gmail.com>
2016-05-21 09:30:45 +0200

    restored villorShow.js, accidentely removed it in last commit

---
+++ www/directives/objects/villorShow.js
@@ -0,0 +1,33 @@
+app.directive('getVillor', [function(){
+	// Runs during compile
+	return {
+		templateUrl: '/directives/objects/show.html',
+		controller : ['$scope','$anchorScroll' ,'Home', function($scope, $anchorScroll, Home){
+			$scope.go = function(capsulate) {
+    			console.log(capsulate);
+    
+				var limit = 6;
+				var skip = capsulate * limit;
+    			console.log(skip);
+				Home.get({
+					type: 'Villor',
+					// _sort: {address: 1}, 
+					_skip: skip,
+  					_limit: limit
+				},function(data,arg){
+				
+					$scope.updown = capsulate;
+					$scope.information = data;
+				})
+
+				$anchorScroll()
+
+  			}
+			$scope.go(0)
+
+
+
+		}]
+	}
+
+}]);
