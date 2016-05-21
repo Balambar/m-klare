096bf6f693749d8feb6ea6843b9800be5d0f68dc
Jarl Holmberg Lindquist <jarllindquist@gmail.com>
2016-05-21 21:30:05 +0200

    fixed the feedback alerts in about -> aboutTabs

--- www/directives/aboutTabs.js
+++ www/directives/aboutTabs.js
@@ -1,6 +1,29 @@
 app.directive('aboutTabs', [function(){
 	// Runs during compile
 	return {
-		templateUrl: '/directives/aboutTabs.html'
+		templateUrl: '/directives/aboutTabs.html',
+		controller : ['$scope','$anchorScroll' , 'Alertify', 'Home', function($scope, $anchorScroll, Alertify, Home){
+				$scope.sendContact = function(){
+		
+		if ($scope.contactName == undefined || $scope.contactEmail == undefined || $scope.contactMessage == undefined) {
+			  Alertify.success("Du måste fylla i alla obligatoriska rutor!");
+
+		}
+		else
+			Contact.create({
+				name: $scope.contactName,
+				email: $scope.contactEmail,
+				phone: $scope.contactPhone,
+				message: $scope.contactMessage,
+				type: "Regular Contact"
+			}, function(){
+				Alertify.success("Tack för din meddelande!");
+				$scope.contactName = undefined;
+				$scope.contactEmail = undefined;
+				$scope.contactPhone = undefined;
+				$scope.contactMessage = undefined;
+			});
+	};
+		}]
  	};
 }]);
