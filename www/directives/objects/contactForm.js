7f8d4ed38d9ab7ab675824b247418b2e8ffdbf35
Jarl Holmberg Lindquist <jarllindquist@gmail.com>
2016-05-22 12:56:01 +0200

    created directives for contact forms, all forms working now

---
+++ www/directives/objects/contactForm.js
@@ -0,0 +1,34 @@
+app.directive('contactForm', [function(){
+
+	return {
+
+		templateUrl: 'directives/objects/contactForm.html',
+		controller: ("contact", ["$scope", 'Alertify', "Contact", function($scope,Alertify ,Contact){
+
+	$scope.sendContact = function(){
+		if ($scope.contactName == undefined || $scope.contactEmail == undefined || $scope.contactMessage == undefined) {
+			  Alertify.success("Du måste fylla i alla obligatoriska rutor!");
+			
+		}
+		else
+			Contact.create({
+				name: $scope.contactName,
+				email: $scope.contactEmail,
+				phone: $scope.contactPhone,
+				message: $scope.contactMessage,
+				type: "Regular Contact"
+			}, function(){
+				Alertify.success("Tack för din meddelande!");
+				$scope.contactName = undefined;
+				$scope.contactEmail = undefined;
+				$scope.contactPhone = undefined;
+				$scope.contactMessage = undefined;
+			});
+	};
+
+}])
+	
+
+	}
+
+}]);
